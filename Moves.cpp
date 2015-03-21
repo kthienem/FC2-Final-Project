403d9b1bc670748ed9c7f85c811965da343238b8
Patrick Ryan Drumm <pdrumm@student02.cse.nd.edu>
2015-03-21 16:35:46 -0400

    base outline of the Pokemon and Moves classes

---
+++ Moves.cpp
@@ -0,0 +1,59 @@
+#include "Moves.h"
+
+enum moveList{
+	MOVE_TACKLE,
+	MOVE_SCRATCH,
+	MOVE_EMBER,
+	MOVE_WATERGUN,
+	MOVE_CUT,
+	MOVE_SHOCK
+};
+
+Moves::Moves() {
+
+}
+
+void Moves::attack(int attackStat) {
+	switch(myAttack) {
+		case: MOVE_TACKLE
+			tackle(attackStat);
+			break;
+		case: MOVE_SCRATCH
+			scratch(attackStat);
+			break;
+		case: MOVE_EMBER
+			ember(attackStat);
+			break;
+		case: MOVE_WATERGUN
+			watergun(attackStat);
+			break;
+		case: MOVE_CUT
+			cut(attackStat);
+			break;
+		case: MOVE_SHOCK
+			shock(attackStat);
+			break;
+		default:
+			noMove();
+			break;
+	}
+}
+
+void levelUp() {
+	// do something to set myMove = to new value
+}
+
+void Moves::noMove(int attackStat){};
+
+int Moves::tackle(int attackStat){};
+
+int Moves::scratch(int attackStat){};
+
+int Moves::ember(int attackStat){};
+
+int Moves::watergun(int attackStat){};
+
+int Moves::cut(int attackStat){};
+
+int Moves::shock(int attackStat){};
+
